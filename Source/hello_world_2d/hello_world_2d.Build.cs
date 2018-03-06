a45e1ee8ce1e529b1259dedd95699f8cffb248da
David Lovi <dlovi@ualberta.ca>
2018-03-05 23:35:54 -0700

    Add RHI (Rendering Hardware Interface) as a build dependency
    The next commit will use it to query screen resolution information.

--- Source/hello_world_2d/hello_world_2d.Build.cs
+++ Source/hello_world_2d/hello_world_2d.Build.cs
@@ -8,7 +8,7 @@ public class hello_world_2d : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "RHI" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
